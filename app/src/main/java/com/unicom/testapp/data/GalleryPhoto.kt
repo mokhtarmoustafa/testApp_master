4760ac12352cc91958f59d14bd771d7de3959c07
m.elmostafa <m.elmostafa@uniicomg.com>
2020-12-01 13:45:55 +0200

    add gallery

---
+++ app/src/main/java/com/unicom/testapp/data/GalleryPhoto.kt
@@ -0,0 +1,21 @@
+package com.unicom.testapp.data
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class GalleryPhoto(
+    val id: String,
+    val description: String?,
+    val user: UserPhoto,
+    val currentUserCollection: CurrentUserCollection
+) : Parcelable {
+    @Parcelize
+    data class UserPhoto(val id: String, val name: String, val username: String?) : Parcelable {
+    }
+
+    @Parcelize
+    data class CurrentUserCollection(val raw: String, val full: String) : Parcelable {}
+}
+
+
